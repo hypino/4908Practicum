30c7e95f02471e0bf73ad997870921ee06e3dfc3
Tyler <hypino@Hypino.(none)>
2012-05-04 12:24:22 -0700

    Made local socket listen

--- src/server/SensorDataCollector.py
+++ src/server/SensorDataCollector.py
@@ -33,6 +33,7 @@ class SensorDataCollector(threading.Thread):
             pass
         #Create the socket at the file location
         self.__localSocket.bind(LOCALDATA)
+        self.__localSocket.listen(1)
         self.__localSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.run()
         
